4ce47bdc0923936dc0d33b42a5f85f1837b0e946
Jack Clarke <jackfulcher09@gmail.com>
2021-12-14 18:36:47 -0500

    missed part 2 of day 1

--- day1/sonar-sweep.clj
+++ day1/sonar-sweep.clj
@@ -2,19 +2,40 @@
 (require '[clojure.string :as str])
 (require '[clojure.edn :as edn])
 
-(print (reduce 
-  (fn [prevMap current] 
-    (update 
-      (if 
-        (< 
-          (:prev prevMap) 
-          current) 
-        (update prevMap :sum #(inc %))
-        prevMap) 
-    :prev (fn [_] current)))
-
-
-  {:prev 100000000000 :sum 0} 
-
-  (map #(edn/read-string %) 
+(def parsed-input
+(vec (map #(edn/read-string %)
        (clojure.string/split-lines input))))
+
+(defn count-increases [data]
+  (:sum
+   (reduce
+  (fn [prevmap current]
+    (update
+      (if
+        (<
+          (:prev prevmap)
+          current)
+        (update prevmap :sum #(inc %))
+        prevmap)
+      :prev
+      (fn [_] current)))
+  {:prev 100000000000 :sum 0}
+  data)
+  ))
+
+(def solution1
+ (count-increases parsed-input))
+
+
+(def input-len (count parsed-input))
+(def windowed-input (map-indexed
+                     (fn [index num]
+                       (if (< (- input-len 3) index)
+                            0
+                            (+
+                             num
+                             (get parsed-input (inc index))
+                             (get parsed-input (+ 2 index)))))
+                      parsed-input))
+
+(def solution2 (count-increases windowed-input))
